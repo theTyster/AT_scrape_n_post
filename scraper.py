f4dd3605bada39618f917eb32dce7d848b9aecf8
T <noreply@mylegendary.quest>
2023-05-09 17:48:05 -0700

    Succesfully scraping image url's. Testing to make sure we are not missing any.
     Changes to be committed:
    	modified:   scraper.py
    	deleted:    test_scrapes/cover_gallery_div__scrape.txt
    	deleted:    test_scrapes/cover_gallery_noscript__scrape.txt
    	deleted:    test_scrapes/current_scrape.txt
    	deleted:    test_scrapes/soup.html
    	deleted:    testing_file.py

--- scraper.py
+++ scraper.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python3
 
 import os, re, requests
+from pathlib import Path
 from bs4 import BeautifulSoup
+
+# path that scraped data will write out to will be the parent folder of this repo.
+p = Path(__file__).parents[1]
+if os.path.exists(f'{p}/scraped'):
+    print('Scrape directory already exists, overwriting.')
+else:
+    os.mkdir(f'{p}/scraped')
+
 
 # Request the fandom page.
 root_url = "https://adventuretime.fandom.com"
@@ -12,31 +21,27 @@ soup = BeautifulSoup(request.text, 'html.parser')
 issues_a = soup.find_all('a', href=re.compile(r'.*issue', flags=re.I))
 issues = set()
 
+
 for i in issues_a:
     issues_a_href = i.get('href')
     issues.add(issues_a_href.replace('/wiki/', root_url + '/wiki/'))
 
-check = []
+
 for i in issues:
-    print(i)
     request = requests.get(i)
     soup = BeautifulSoup(request.text, 'html.parser')
 
     cover_gallery_span = soup.find('span', id='Gallery')
-    for span in cover_gallery_span:
-        cover_gallery_h2 = span.find_parent('h2')
-        cover_gallery_h3 = cover_gallery_h2.find_next(id=re.compile(r'cover.*', flags=re.I))
-        cover_gallery_div = cover_gallery_h3.find_next('div')
-        cover_gallery_noscript = cover_gallery_div.find_all('noscript')
-        for noscript in cover_gallery_noscript: 
-            cover_img = noscript.img.get('src')
-            cover_url_split = cover_img.split('jpg', maxsplit=1)
-            print(cover_url_split)
-            cover_fullsize = cover_url_split[0] + cover_url_split[1]
+    cover_gallery_h2 = cover_gallery_span.find_parent('h2')
+    cover_gallery_h3 = cover_gallery_h2.find_next(id=re.compile(r'cover.*', flags=re.I))
+    cover_gallery_div = cover_gallery_h3.find_next('div')
+    cover_gallery_noscript = cover_gallery_div.find_all('noscript')
 
 
-            check.append(cover_img)
-        print(len(check))
+    for noscript in cover_gallery_noscript: 
+        cover_img = noscript.img.get('src')
+        cover_url_split = re.split(r'(.png|.gif|.jpg|.jpeg|.webp)', cover_img,  maxsplit=1, flags=re.I)
+        cover_fullsize = cover_url_split[0] + cover_url_split[1]
 
-print('Issues: ' + str(len(issues)))
-print('Items: ' + str(len(issues)))
+        scraped = open(f'{p}/scraped/fullsize_covers.csv', 'a')
+        scraped.write(cover_fullsize + ', \n')
