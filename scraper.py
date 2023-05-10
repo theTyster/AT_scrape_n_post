c650c25f8a31c2b8d183b723ebe5d87f63332b9f
T <noreply@mylegendary.quest>
2023-05-09 17:16:34 -0700

    working on modifying cover_url_split to split on any image extension not just .jpg commit early commit often.
     Changes to be committed:
    	modified:   scraper.py
    	new file:   test_scrapes/cover_gallery_div__scrape.txt
    	new file:   test_scrapes/cover_gallery_noscript__scrape.txt

--- scraper.py
+++ scraper.py
@@ -16,11 +16,27 @@ for i in issues_a:
     issues_a_href = i.get('href')
     issues.add(issues_a_href.replace('/wiki/', root_url + '/wiki/'))
 
+check = []
 for i in issues:
+    print(i)
     request = requests.get(i)
     soup = BeautifulSoup(request.text, 'html.parser')
 
-    cover_gallery_span = soup.find_all('span', id='Gallery')#re.compile('cover.*', flags=re.I))
-    cover_gallery_h2 = cover_gallery_span.find_parent('h2')
-    cover_gallery_h3 = cover_gallery_h2.find_next_sibling('h3')
-    cover_gallery_div = cover_gallery_h3.find_next_sibling('div')
+    cover_gallery_span = soup.find('span', id='Gallery')
+    for span in cover_gallery_span:
+        cover_gallery_h2 = span.find_parent('h2')
+        cover_gallery_h3 = cover_gallery_h2.find_next(id=re.compile(r'cover.*', flags=re.I))
+        cover_gallery_div = cover_gallery_h3.find_next('div')
+        cover_gallery_noscript = cover_gallery_div.find_all('noscript')
+        for noscript in cover_gallery_noscript: 
+            cover_img = noscript.img.get('src')
+            cover_url_split = cover_img.split('jpg', maxsplit=1)
+            print(cover_url_split)
+            cover_fullsize = cover_url_split[0] + cover_url_split[1]
+
+
+            check.append(cover_img)
+        print(len(check))
+
+print('Issues: ' + str(len(issues)))
+print('Items: ' + str(len(issues)))
