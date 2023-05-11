de1dfa785ae6eeb12579c0b3ea98329bf287a35f
T <noreply@mylegendary.quest>
2023-05-11 00:46:55 -0700

    bmo is born.
     Changes to be committed:
    	modified:   poster.py
    	new file:   register.py

--- poster.py
+++ poster.py
@@ -1,23 +1,22 @@
 #! /usr/bin/env python3
 
 from mastodon import Mastodon
-from auth import (password, akkoma_access_token, client_secret)
+from auth import *
 
 
 
 
 def post(msg):
-    #log in
-    akkoma = Mastodon(client_id = client_secret)
-    akkoma.log_in('cover_art_time', password, to_file = akkoma_access_token)
-
-    akkoma = Mastodon(access_token = akkoma_access_token)
+    akkoma = Mastodon(api_base_url = 'https://thisis.mylegendary.quest', access_token = akkoma_access_token)
     akkoma.toot(msg)
 
 post('''
 ALGEBRAIC
+
 A whole federated universe of people sharing stuff and doing things!
-I can't wait to share all the cool Comic book cover art that I have here with me.
+
+I cannot wait to dip my toes in the salsa and share all the cool Comic book cover art that I have here with me.
+
 You can view my source code at https://github.com/twizzay-code/AT_scrape_n_post.
 @twizzay is my papa. <3
 ''')
---
+++ register.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+from mastodon import Mastodon
+from auth import *
+
+Mastodon.create_app('Adventure Time Comics', api_base_url = 'https://thisis.mylegendary.quest', to_file = 'clientcreds.secret')
+
+Mastodon(client_id = 'clientcreds.secret').log_in('bmo', password, to_file = 'usercred.secret')
