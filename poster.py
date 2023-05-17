748bf8930a97373d5e7f9e69baa29b9c8a457246
T <noreply@mylegendary.quest>
2023-05-17 15:47:39 -0700

    decorators were learned about and discovered to be overkill. The API has documentation was utilized to create a function with default parameters that can be called from the REPL
     Changes to be committed:
    	modified:   poster.py

--- poster.py
+++ poster.py
@@ -1,22 +1,10 @@
 #! /usr/bin/env python3
-
 from mastodon import Mastodon
-from auth import *
+from auth import akkoma_access_token
 
+def status(*args, **kwargs):
+    akko = Mastodon(api_base_url = 'https://thisis.mylegendary.quest', access_token = akkoma_access_token)
+    akko.feature_set = 'pleroma'
+    akko.status_post(*args, visibility='direct', language='en', content_type='text/markdown', **kwargs)
 
-
-
-def post(msg):
-    akkoma = Mastodon(api_base_url = 'https://thisis.mylegendary.quest', access_token = akkoma_access_token)
-    akkoma.toot(msg)
-
-post('''
-ALGEBRAIC
-
-A whole federated universe of people sharing stuff and doing things!
-
-I cannot wait to dip my toes in the salsa and share all the cool Comic book cover art that I have here with me.
-
-You can view my source code at https://github.com/twizzay-code/AT_scrape_n_post.
-@twizzay is my papa. <3
-''')
+status('check, please')
