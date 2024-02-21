1fc672a5c9cf165952f75112db0860ee44292447
T <noreply@mylegendary.quest>
2024-02-21 13:50:12 -0800

    msg array in separate file for easier management.

---
+++ code/msg.py
@@ -0,0 +1,104 @@
+msg_text = [
+f"""
+oh my glob. :at_bongocatbmo:
+
+Look. at this rad comic book cover I found. :at_JakeTheDog_heartEyes:
+""",
+f"""
+Sometimes when I am sad like finn :at_FinnTheBoy_cry: ,
+I just look at cool arts.
+""",
+f"""
+I am beemo.
+One day you will be old and also you will be dead.
+
+But it's ok, because I will still be here posting cool pictures for you.
+""",
+f"""
+What time is it??
+It's not adventure time.
+That's not what the internet is for I dont think.
+
+But, that is ok because it is comic book time!
+:at_AdventureTime:
+:at_FinnJake_fistbump:
+""",
+f"""
+I do not post anything that is not either cool and/or awesome.
+""",
+f"""
+Did you know that I can ollie over a sandwhich *while* looking awesome.
+Ok, I only did that one time. And, yes,  there was a ramp. >.>
+""",
+f"""
+Hello. word.
+
+:at_bmoWave:
+""",
+f"""
+I shall never enjoy the pleasures of skinny dipping.
+
+But, I do enjoy cool looking pictures.
+""",
+f"""
+Did you know I have a brother named allmo? :at_all_mo:
+He's pretty cool.
+
+Anyway, here's a picture or something.
+""",
+"""
+Comics are like. The best.
+""",
+"""
+Hey there Mr. Tuff pants, do you think you have what it takes to go toe to toe with The BMO in a comic book cover recollection tournament?
+
+[Try me, Hot Stuff](https://mylegendary.quest/unlisted/Memory_Game/index.html).
+""",
+"""
+What's colorful and pretty and red all over?
+A comic!
+""",
+"""
+To be honest, I think this one might be my new favorite.
+""",
+"""
+\*kickflip\* 🛹
+""",
+"""
+The votes are in. BMO is the new president.
+As my first order of business, I declare this hour to be mandatory comic book reading hour.
+""",
+"""
+Well this is awkward. You were looking for something boring to comment on, but all you found was this radical picture. ¯\\\_(ツ)\_/¯
+""",
+"""
+So, now that you have seen my cool picture, what should we do now?
+""",
+"""
+Do you ever think that maybe there is more to life than posting pictures on the internet?
+... :thinking:
+Nah.
+""",
+"""
+Boy, some of you guys sure do have a lot of words to say.
+""",
+"""
+LEEEEEEEEEROOOOYYY
+""",
+"""
+| (• ◡•)|     (❍ᴥ❍ʋ)
+""",
+"""
+I saw a dog today.
+""",
+"""
+When one see's a lion, one must get into the car.
+""",
+"""
+buzz ping! Your copypasta is ready! Oh, I just love italy.
+""",
+"""
+I found this buried deep in the restricted section of my hard drive. 🙀
+"""
+
+]
